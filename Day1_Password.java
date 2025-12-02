49462ea398c063a7861778897e2c5d83db779783
Nehal Solanki <117564486+nehalss267@users.noreply.github.com>
2025-12-02 18:45:00 +0530

    Create Day1_Password.java

---
+++ Day1_Password.java
@@ -0,0 +1,1 @@
+// Puzzle link: https://x.com/intent/tweet?text=%22Secret+Entrance%22+%2D+Day+1+%2D+Advent+of+Code+2025&url=https%3A%2F%2Fadventofcode%2Ecom%2F2025%2Fday%2F1&related=ericwastl&hashtags=AdventOfCode
