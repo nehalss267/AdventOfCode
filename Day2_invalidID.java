765299b952013efe408d734e9614a31ab0c52c4f
Nehal <nehalss267@gmail.com>
2025-12-02 21:22:36 +0530

    Updating code

--- Day2_invalidID.java
+++ Day2_invalidID.java
@@ -1,1 +1,23 @@
+
 //
+import java.util.*;
+import java.io.*;
+
+public class Day2_invalidID {
+    public static void palindrome(String data){
+        int intData=Integer.parseInt(data);
+        int len=data.length();
+        if()
+    }
+
+    public static void main(String[] args) {
+        File input = new File("F:\\1-sem4\\DSA\\CodingChallenges\\AdventOfCode\\input2.txt");
+        try (Scanner sc = new Scanner(input)) {
+            while (sc.hasNextLine()) {
+                String data = sc.nextLine();
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println(e);
+        }
+    }
+}
