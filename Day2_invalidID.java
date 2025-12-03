9a515ff335b0b1f88e6e68a88fd1bec23d138ade
Nehal <nehalss267@gmail.com>
2025-12-03 23:52:04 +0530

    Updating code

--- Day2_invalidID.java
+++ Day2_invalidID.java
@@ -1,23 +1,57 @@
-
-//
 import java.util.*;
 import java.io.*;
 
 public class Day2_invalidID {
-    public static void palindrome(String data){
-        int intData=Integer.parseInt(data);
-        int len=data.length();
-        if()
+    static int count = 0;
+    static Long sum = 0L;
+    // static Set<Long> invalidIDList = new HashSet<>();
+    static List<Long> invalidIDList = new ArrayList<>();
+
+    public static void doubleSequence(String data) {
+        int len = data.length();
+        if ((len % 2) != 0) {
+            return;
+        }
+        String l = data.substring(0, (len / 2));
+        String r = data.substring((len / 2));
+        if (!l.equals(r)) {
+            return;
+        }
+        Long LongData = Long.parseLong(data);
+        invalidIDList.add(LongData);
+
+    }
+
+    public static void range(String data) {
+        String dataArr[] = data.split("-");
+        for (long i = Long.parseLong(dataArr[0]); i <= Long.parseLong(dataArr[1]); i++) {
+            doubleSequence(String.valueOf(i));
+        }
     }
 
     public static void main(String[] args) {
-        File input = new File("F:\\1-sem4\\DSA\\CodingChallenges\\AdventOfCode\\input2.txt");
+        String data = " ";
+        File input = new File("F:\\1-sem4\\DSA\\CodingChallenges\\AdventOfCode_local\\input2.txt");
         try (Scanner sc = new Scanner(input)) {
             while (sc.hasNextLine()) {
-                String data = sc.nextLine();
+                data = sc.nextLine();
             }
         } catch (FileNotFoundException e) {
             System.out.println(e);
         }
+        String dataArr[] = new String[data.length()];
+
+        dataArr = data.split(",");// 0 of arr
+        for (String elem : dataArr) {
+            range(elem);
+        }
+
+        for (Long elem : invalidIDList) {
+            sum += elem;
+            count++;
+        }
+        System.out.println(count);
+        System.out.println(sum);
+        System.out.println(invalidIDList);
     }
 }
